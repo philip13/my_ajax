95a977a3a46c82821330b05cf8db45b0f7c0af4d
Phillip Cuevas <felipe@softwareallies.com>
2017-03-10 10:15:19 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyAjax::Application.config.secret_token = '84138674a320fbc402c46211ad5163c7548ab3be62b5f72df1fb234d3f040b10fed2efee5f2c23520cd5518491b30cfd272574a60069c9ce2e96e102d7d8d84e'
